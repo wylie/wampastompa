380cdf8ae7673217c15e2e2729ca4cdf774af130
Wylie <me@wyliefisher.com>
2024-11-08 12:21:24 -0500

    had the wrong path to the JS

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const txt = document.getElementById('txt');
 const loc = window.location.hash;
 const hash = loc.split('#');
-console.log( hash );
 const nam = hash[1].replace(/[-_\s]/g, " ");
 txt.innerHTML = nam;
