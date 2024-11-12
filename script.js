844662e775d12a96f8b49cfbf510cb102b044554
Wylie Fisher <me@wyliefisher.com>
2024-11-12 14:02:41 -0500

    lets test these changes out

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-const txt = document.getElementById('txt');
-console.log(txt);
-const loc = window.location.hash;
-console.log(loc);
-const [_, nam] = loc.split('#');
+document.addEventListener('DOMContentLoaded', () => {
+  const txt = document.getElementById('txt');
+  const input = document.getElementById('mixName');
 
-if (txt && nam) {
-  txt.innerHTML = nam.replace(/[-_\s]/g, " ");
-}
+  input.addEventListener('input', () => {
+    const nam = input.value;
+
+    if (txt && nam) {
+      txt.innerHTML = nam.replace(/[-_\s]/g, " ");
+    }
+  });
+
+  console.log('html2canvas in script.js:', typeof html2canvas);
+
+  if (typeof html2canvas !== 'undefined') {
+    document.getElementById('downloadBtn').addEventListener('click', function() {
+      const imgElement = document.querySelector('.img');
+      html2canvas(imgElement).then(canvas => {
+        const link = document.createElement('a');
+        link.href = canvas.toDataURL('image/png');
+        link.download = 'wampastompa.png';
+        link.click();
+      });
+    });
+  } else {
+    console.error('html2canvas is not defined');
+  }
+});
