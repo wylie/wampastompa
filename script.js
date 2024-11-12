8245ad45c7ae343f4a39d512be1a591dae2a1236
Wylie Fisher <me@wyliefisher.com>
2024-11-12 10:23:49 -0500

    now we defer!

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const txt = document.getElementById('txt');
-  console.log(txt);
-  const loc = window.location.hash;
-  console.log(loc);
-  const [_, nam] = loc.split('#');
+const txt = document.getElementById('txt');
+console.log(txt);
+const loc = window.location.hash;
+console.log(loc);
+const [_, nam] = loc.split('#');
 
-  if (txt && nam) {
-    txt.innerHTML = nam.replace(/[-_\s]/g, " ");
-  }
-});
+if (txt && nam) {
+  txt.innerHTML = nam.replace(/[-_\s]/g, " ");
+}
